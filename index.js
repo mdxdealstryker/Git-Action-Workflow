f5b4aef5e5e3f7ae1a9d70cb71d8c9140b94a04b
unknown <you@example.com>
2024-05-30 10:51:58 -0400

    updated

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!"); 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Git Devops by Git Actions !!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
